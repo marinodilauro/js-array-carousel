e95b8e523c04aa21f73d7827fcdd36fe13b786b1
marinodilauro <m.dilauro90@gmail.com>
2024-02-23 14:17:09 +0100

    Add js initial varaibles

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Create a variable for the list of image names
+
+const imageNames = [
+  "01.jpg",
+  "02.jpg",
+  "03.jpg",
+  "04.jpg",
+  "05.jpg",
+];
+
+
+// Create a variable for the image displayed on screen
+
+let activeImage = 0;
+
+
+// Create a variable for HTML slide element
+
+const slideElement = document.querySelector(".slides");
